7be18f42334abf4a720ef5f17413b9fb922992bf
OrMoka <nathanael.verdier@etu.univ-st-etienne.fr>
2022-01-14 00:21:59 +0100

    commit

--- src/Controller/GrandControlleurController.php
+++ src/Controller/GrandControlleurController.php
@@ -9,18 +9,60 @@ use Symfony\Component\Routing\Annotation\Route;
 class GrandControlleurController extends AbstractController
 {
     /**
-     * @Route("/grand/controlleur", name="grand_controlleur")
+     * @Route("/grandcontrolleur", name="grand_controlleur")
      */
     public function index(): Response
     {
         return $this->render('grand_controlleur/index.html.twig');
     }
     /**
-     * @Route("/", name="home")
+     * @Route("/Accueil", name="accueil")
      */
 	public function home(): Response
 	{
 		return $this->render('grand_controlleur/Accueil.html.twig');
 	}
-	
+    /**
+     * @Route("/CV", name="cv")
+     */
+	public function CV(): Response
+	{
+		return $this->render('grand_controlleur/CV.html.twig');
+	}
+	/**
+     * @Route("/Portfolio", name="folio")
+     */
+	public function port(): Response
+	{
+		return $this->render('grand_controlleur/Folio.html.twig');
+	}
+    /**
+     * @Route("/Adminitrer1", name="adminS1")
+     */
+	public function admin1(): Response
+	{
+		return $this->render('grand_controlleur/amdininistrerpre.html.twig');
+	}
+    /**
+     * @Route("/ConnecterS1", name="connectS1")
+     */
+	public function admin1(): Response
+	{
+		return $this->render('grand_controlleur/connecterpre.html.twig');
+	}
+    /**
+     * @Route("/Programmation1", name="progS1")
+     */
+	public function admin1(): Response
+	{
+		return $this->render('grand_controlleur/programmerrpre.html.twig');
+	}
+    /**
+     * @Route("/Passion", name="passion")
+     */
+	public function passion(): Response
+	{
+		return $this->render('grand_controlleur/Passion.html.twig');
+	}
+
 }
